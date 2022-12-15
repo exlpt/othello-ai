e6eef6139e5e3d9cc25fc8d52fba6eec3df3f1ac
Josiah <jo.launchpad@gmail.com>
2022-12-15 15:37:09 -0500

    Declare game logic functions

---
+++ othelloFuncs.js
@@ -0,0 +1,13 @@
+export function moveOnBoard(board, move) {
+}
+
+export function scoreBoard(board) {
+}
+
+// returns 0 for game not finished, 1 for black win, -1 for white win
+export function gameIsFinished(board) {
+}
+
+export function moveIsAvailable(board, move) {
+	
+}
