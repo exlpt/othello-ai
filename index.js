66cd85c51ddeeb8637700c0a5bc3e4ecb8e8a4fa
Josiah <jo.launchpad@gmail.com>
2022-12-17 16:50:11 -0500

    Add board UI

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 /* TODO ============
- - Finish declaring ui functions
- - Create ui in index
+ - Add UI handlers
 */
+
+const boardContainer = document.createElement("div");
+boardContainer.setAttribute("class", "container");
+document.querySelector(".content").appendChild(boardContainer);
+
+for (let row = 0; row < 8; row++) {
+	for (let col = 0; col < 8; col++) {
+		const cell = document.createElement("div");
+		cell.setAttribute("class", `board_cell cell_${row}-${col}`);
+		boardContainer.appendChild(cell);
+	}
+}
