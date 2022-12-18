d529e6c8f668c59e66077a722c0f51d80ff2cab3
Josiah <jo.launchpad@gmail.com>
2022-12-18 11:20:54 -0500

    Implement all othelloFuncs.js

--- othelloFuncs.js
+++ othelloFuncs.js
@@ -109,9 +109,87 @@ export function moveOnBoard(board, move) {
 	return newBoard;
 }
 
-export function scoreBoard(board) {}
+export function scoreBoard(board) {
+	return board.reduce(
+		(score, row) =>
+			score + row.reduce((rowSore, current) => rowSore + (current == "B" ? 1 : current == "W" ? -1 : 0), 0),
+		0
+	);
+}
 
-// returns 0 for game not finished, 1 for black win, -1 for white win
-export function gameIsFinished(board) {}
+export function gameIsFinished(board) {
+	for (let row = 0; row < board.length; row++) {
+		for (let col = 0; col < board[0].length; col++) {
+			if (board[row][col] == "_") return false;
+		}
+	}
+	return true;
+}
 
-export function moveIsAvailable(board, move) {}
+export function moveIsAvailable(board, move) {
+	// Check for hits upwards
+	for (let currentY = move.y - 1; currentY >= 0; currentY--) {
+		if (board[currentY][move.x] == "_") break;
+		if (board[currentY][move.x] == move.player) return true;
+	}
+
+	// Check for hits downwards
+	for (let currentY = move.y + 1; currentY < board.length; currentY++) {
+		if (board[currentY][move.x] == "_") break;
+		if (board[currentY][move.x] == move.player) return true;
+	}
+
+	// Check for hits to the left
+	for (let currentX = move.x - 1; currentX >= 0; currentX--) {
+		if (board[move.y][currentX] == "_") break;
+		if (board[move.y][currentX] == move.player) return true;
+	}
+
+	// Check for hits to the right
+	for (let currentX = move.x + 1; currentX < board[0].length; currentX++) {
+		if (board[move.y][currentX] == "_") break;
+		if (board[move.y][currentX] == move.player) return true;
+	}
+
+	// Check for hits to the top left
+	for (
+		let currentPos = [move.x - 1, move.y - 1];
+		currentPos[0] >= 0 && currentPos[1] >= 0;
+		currentPos[0]--, currentPos[1]--
+	) {
+		if (board[currentPos[1]][currentPos[0]] == "_") break;
+		if (board[currentPos[1]][currentPos[0]] == move.player) return true;
+	}
+
+	// Check for hits to the bottom right
+	for (
+		let currentPos = [move.x + 1, move.y + 1];
+		currentPos[0] < board[0].length && currentPos[1] < board.length;
+		currentPos[0]++, currentPos[1]++
+	) {
+		if (board[currentPos[1]][currentPos[0]] == "_") break;
+		if (board[currentPos[1]][currentPos[0]] == move.player) return true;
+	}
+
+	// Check for hits to the top right
+	for (
+		let currentPos = [move.x + 1, move.y - 1];
+		currentPos[0] < board[0].length && currentPos[1] >= 0;
+		currentPos[0]++, currentPos[1]--
+	) {
+		if (board[currentPos[1]][currentPos[0]] == "_") break;
+		if (board[currentPos[1]][currentPos[0]] == move.player) return true;
+	}
+
+	// Check for hits to the bottom left
+	for (
+		let currentPos = [move.x - 1, move.y + 1];
+		currentPos[0] >= 0 && currentPos[1] < board.length;
+		currentPos[0]--, currentPos[1]++
+	) {
+		if (board[currentPos[1]][currentPos[0]] == "_") break;
+		if (board[currentPos[1]][currentPos[0]] == move.player) return true;
+	}
+
+	return false;
+}
