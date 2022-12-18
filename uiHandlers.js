700e3faf9756f55d7598504e3106c32ead0b2955
Josiah <jo.launchpad@gmail.com>
2022-12-17 22:42:35 -0500

    Declare UI handlers

---
+++ uiHandlers.js
@@ -0,0 +1,8 @@
+import * as othelloFuncs from "./othelloFuncs.js";
+import * as uiFuncs from "./uiFuncs.js";
+
+export function handleCellClick() {
+	// move on current board and set board
+	// toggle currentPlayer
+	// update ui
+}
